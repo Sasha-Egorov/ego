25ecb742b8ec3126ba64ac742c1fc55b80145621
Sasha-Egorov <73832586+Sasha-Egorov@users.noreply.github.com>
2021-01-17 18:04:29 +0300

    Hw 4 - task 2 Palindrome

---
+++ src/main/java/com/egorov/homeworks/homework04/Palindrome.java
@@ -0,0 +1,37 @@
+package com.egorov.homeworks.homework04;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        printMessage("Enter your sentence: ");
+        String text = scan();
+        printMessage(checkSentence(text));
+
+    }
+
+    private static String scan() {
+        Scanner scan = new Scanner(System.in);
+        String number = scan.nextLine();
+        return number;
+    }
+
+    private static void printMessage(String text) {
+        System.out.println(text);
+
+    }
+
+    private static String checkSentence(String text) {
+        text = text.toUpperCase(Locale.ROOT);
+        text = text.replaceAll("\\s+", "");
+        char[] array = text.toCharArray();
+        System.out.println(array.length);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != array[array.length - i - 1]) {
+                return "Sentence is not palindrome";
+            }
+        }
+        return "Sentence is palindrome";
+    }
+}
