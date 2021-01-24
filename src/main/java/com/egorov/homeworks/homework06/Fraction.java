e3dc602551f43383d6be342ee3d5179daa17eb24
Sasha-Egorov <73832586+Sasha-Egorov@users.noreply.github.com>
2021-01-24 15:18:06 +0300

    Hw 6 Interface

---
+++ src/main/java/com/egorov/homeworks/homework06/Fraction.java
@@ -0,0 +1,8 @@
+package com.egorov.homeworks.homework06;
+
+public interface Fraction {
+    FractionNumber plus(FractionNumber numb);
+    FractionNumber minus(FractionNumber numb);
+    FractionNumber multiply(FractionNumber numb);
+    FractionNumber divide(FractionNumber numb);
+}
