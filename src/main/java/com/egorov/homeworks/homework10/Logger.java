251acff85a8cf18168c1d2aa7b1152eee71e8291
Sasha-Egorov <73832586+Sasha-Egorov@users.noreply.github.com>
2021-02-07 20:31:56 +0300

    Homework 10 Logger

---
+++ src/main/java/com/egorov/homeworks/homework10/Logger.java
@@ -0,0 +1,30 @@
+package com.egorov.homeworks.homework10;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public abstract class Logger {
+    Level level;
+    String message;
+
+    public Logger(Level level, String message) {
+        this.level = level;
+        this.message = message;
+    }
+
+    public String createMessage(String message, Level level) {
+
+        String result = "" + LocalDate.now() + " " + LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute() + ":" + LocalDateTime.now().getSecond();
+        result = result + " " + level.name() + ": " + message + "\n";
+        return result;
+    }
+
+    public void log(String message) {
+
+    }
+
+    public void log(String message, Level level) {
+
+    }
+
+}
