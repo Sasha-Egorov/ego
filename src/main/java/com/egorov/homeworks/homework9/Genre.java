35e084b98313c6541b99ce88032030f50fa680fb
Sasha-Egorov <73832586+Sasha-Egorov@users.noreply.github.com>
2021-02-07 11:36:48 +0300

    Homework 9 Enum

---
+++ src/main/java/com/egorov/homeworks/homework9/Genre.java
@@ -0,0 +1,13 @@
+package com.egorov.homeworks.homework9;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public enum Genre {
+
+    Fantasy("Fantasy"), HORROR("Horror"), ROMANCE("Romance"), COOKBOOKS("Cookbooks"), FAIRY_TALE("Fairy tale");
+
+    @Getter
+    private final String name;
+}
