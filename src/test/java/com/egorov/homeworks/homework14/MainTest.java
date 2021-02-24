63f74f3d9cc989b165eb7340cce4132391ea4aa0
Sasha-Egorov <73832586+Sasha-Egorov@users.noreply.github.com>
2021-02-25 01:58:46 +0300

    Homework 14 Test

--- src/test/java/com/egorov/homeworks/homework14/MainTest.java
+++ src/test/java/com/egorov/homeworks/homework14/MainTest.java
@@ -10,21 +10,25 @@ class MainTest {
         CovidStat stat = new CovidStat(new DataProvider().provide());
         assertEquals(41, stat.getAverageAge());
     }
+
     @Test
     void testAverageHeight() {
         CovidStat stat = new CovidStat(new DataProvider().provide());
         assertEquals(165.75, stat.getAverageHeight());
     }
+
     @Test
     void testAverageWeight() {
         CovidStat stat = new CovidStat(new DataProvider().provide());
         assertEquals(77.5, stat.getAverageWeight());
     }
+
     @Test
     void testPopularMailDomain() {
         CovidStat stat = new CovidStat(new DataProvider().provide());
         assertEquals("gmail.com", stat.getPopularMailDomain());
     }
+
     @Test
     void testPopularMailName() {
         CovidStat stat = new CovidStat(new DataProvider().provide());
