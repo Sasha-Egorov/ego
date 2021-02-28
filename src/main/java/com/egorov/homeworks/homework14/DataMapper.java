d4854a150c33c95162a282392115cac87670b1fc
Sasha-Egorov <73832586+Sasha-Egorov@users.noreply.github.com>
2021-03-01 00:06:40 +0300

    Homework 15 DataMapper

--- src/main/java/com/egorov/homeworks/homework14/DataMapper.java
+++ src/main/java/com/egorov/homeworks/homework14/DataMapper.java
@@ -7,7 +7,15 @@ public class DataMapper {
     public static Person map(String text) {
 
         String[] words = text.split(",");
-        return new Person(words[0], words[1], Integer.parseInt(words[2]), Integer.parseInt(words[3]), Integer.parseInt(words[4]), words[5]);
+        return new Person.PersonBuilder()
+                .name(words[0])
+                .surname(words[1])
+                .age(Integer.parseInt(words[2]))
+                .height(Integer.parseInt(words[3]))
+                .weight(Integer.parseInt(words[4]))
+                .email(words[5])
+                .build();
+
 
     }
 
